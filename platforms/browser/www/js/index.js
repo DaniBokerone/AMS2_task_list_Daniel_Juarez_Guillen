6d63c064c17d273244222791afcdeefc9a5093de
DaniBokerone <danibokerone@gmail.com>
2024-10-22 15:45:34 +0200

    Initial commit
    c

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,60 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    let id = 0; // Inicializamos el contador de ID para las tareas
+
+    // Diálogo de creación de la tarea
+    $("#dialog").dialog({
+        autoOpen: false,
+        modal: true,
+        buttons: {
+            'OK': function () {
+                var task_name = $('input[name="task_name"]').val(); 
+                if (task_name) { 
+                    generateListElement(task_name);
+                    $(this).dialog('close'); 
+                    $('input[name="task_name"]').val(''); 
+                } else {
+                    alert("Afegeix un nom a la tasca, si us plau"); 
+                }
+            },
+            'Cancel': function () {
+                $(this).dialog('close'); 
+            }
+        }
+    });
+
+    $("#add_task").click(function () {
+        $("#dialog").dialog("open");
+    });
+
+    function generateListElement(texte) {
+        id++; 
+        
+        var buttons = generateButtonList(id);
+        var task = "<div id='task_id_" + id + "' class='list_element'><p>" + texte + "</p>" + buttons + "</div>";
+
+        $('#tasques').append(task);
+    }
+
+    // Función para generar los botones de cada tarea
+    function generateButtonList(taskId) {
+        var editBtn = "<button class='edit-task' data-task-id='" + taskId + "'>Editar</button>";
+        
+        var delBtn = "<button class='delete-task' data-task-id='" + taskId + "'>X</button>";
+
+        return "<div>" + editBtn + delBtn + "</div>";
+    }
+
+    //Borrar tasca
+    $(document).on('click', '.delete-task', function () {
+        const taskId = $(this).data('task-id'); 
+        $('#task_id_' + taskId).remove(); 
+    });
+
+    //Editar nom de la tasca
+    $(document).on('click', '.edit-task', function () {
+        const taskId = $(this).data('task-id'); 
+         
+    });
+}
